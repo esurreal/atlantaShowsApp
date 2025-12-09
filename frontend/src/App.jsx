33861e8340138c7797970bc08758273994325ed8
Eric <esurreal@gmail.com>
2025-12-09 15:43:53 -0500

    nextpush

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import EventList from './components/EventList'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'https://railway.com/project/a2435af1-ae22-4ec4-b1c0-44c293f4b261/service/7c3fb67a-9b44-4fb5-a6a2-ecdfb15fa707?environmentId=b1c4908d-97ae-4960-8451-da1a2e44f15e&id=9e5bd0d9-54ca-4fe4-920a-b095f0f8f1f4#deploy'
+const API_BASE = import.meta.env.VITE_API_BASE || 'postgresql://${{PGUSER}}:${{POSTGRES_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:5432/${{PGDATABASE}}'
 
 export default function App(){
   const [events, setEvents] = useState([])
